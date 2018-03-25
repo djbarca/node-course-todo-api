ddc7a199570a16bd0dea42ff0c5e94f3e0af18b2
David Maposa <08arsenal@gmail.com>
2018-03-24 23:50:13 -0400

    Add queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,31 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+
+var id = '5ab5bb308a754994c8f97cb7';
+
+if (!ObjectID.isValid()) {
+    console.log('ID not valid');
+}
+
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log('Todos', todos);
+// });
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log('Todos', todo);
+// });
+
+Todo.findById(id).then((todo) => {
+    if (!todo) {
+        return console.log('Id not found');
+    }
+    console.log('Todo By Id', todo);
+}).catch((error) => {
+    console.log(error);
+})
