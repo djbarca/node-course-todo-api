15efc13d554cfd724c88475f886490d9ac0f9e06
David Maposa <dmaposa@mitre.org>
2018-03-27 12:27:09 -0400

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -96,6 +96,25 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    // create a new instance of the user model.
+    var user = new User(body);
+
+    //User.findByToken
+    // user.generateAuthToken
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    })
+    .catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
 app.listen(port, () => {
     console.log(`Started up at port ${port}`);
 });
